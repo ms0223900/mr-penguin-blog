b32b5a09d718a6cbf8dd7f11c4e30e4e610f7b53
penguin-cho <penguin-cho@cloud-latitude.com>
2024-07-06 12:00:24 +0800

    feat: basic layout

--- pages/side-projects/shorten-url.tsx
+++ pages/side-projects/shorten-url.tsx
@@ -1,13 +1,18 @@
 "use client"
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from "next/router";
 
 const ShortenUrl: React.FC = (props) => {
     const router = useRouter();
     console.log("router.query: ", router.query);
+    useEffect(() => {
+
+        //
+    }, [])
     return (
-        <div>
+        <div
+            className={router.query.hash ? "fixed z-[100000] top-0 left-0 z-10 w-full h-full bg-white" : "min-h-[80vh]"}>
             <h1>Shorten Url :)</h1>
             <p>
                 {`Hash: ${router.query.hash}`}

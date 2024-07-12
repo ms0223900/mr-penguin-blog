46ace5ae57a54364d0ae734d8e806ac85930fe11
penguin-cho <penguin-cho@cloud-latitude.com>
2024-07-12 09:56:41 +0800

    feat: update api base url

--- components/ShortenUrl/repo/config.ts
+++ components/ShortenUrl/repo/config.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "https://ancient-chamber-92530-10811f1cc15e.herokuapp.com/api";
+const BASE_URL = "https://shorten-url-app-41a57bab2f7b.herokuapp.com/";
+export const API_BASE_URL = BASE_URL + "api";

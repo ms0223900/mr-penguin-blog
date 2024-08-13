626b399632c1ebee005ebb9bbb5fd5f490a58905
penguin-cho <penguin-cho@cloud-latitude.com>
2024-08-13 14:41:35 +0800

    feat: add accounting page

---
+++ pages/side-projects/accounting-app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const AccountingAppIndex: React.FC = (props) => {
+    return (
+        <div>
+            <h1>Accounting :)</h1>
+
+        </div>
+    );
+};
+
+export default AccountingAppIndex;

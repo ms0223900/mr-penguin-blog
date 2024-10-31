bdb4e5710b86fca7a54a202a06fea674df8ffa5d
penguin-cho <penguin-cho@cloud-latitude.com>
2024-10-31 21:10:32 +0800

    fix: fix compile error

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,91 +1,18 @@
 import { Box, CircularProgress, Container, Typography } from '@mui/material';
-import { SinglePost } from 'common-types';
 import TagLinkItem from 'components/Common/TagLinkItem';
 import { WEB_TITLE } from 'config';
-import queryArticleByTag from 'gql/queryArticleByTag';
-import queryArticleByVal from 'gql/queryArticleByVal';
-import useSearchByVal from 'lib/custom-hooks/useSearchByVal';
-import useToggle from 'lib/custom-hooks/useToggle';
-import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import QueriedArticleHandlers from 'lib/handlers/QueriedArticleHandlers';
-import React, { memo, useEffect, useState } from 'react';
+import React, { memo } from 'react';
 import Script from 'next/script';
 import PostList, {
   getStaticProps,
   PostListViewProps,
-  // getServerSideProps,
 } from 'components/Post/PostList';
 export { getStaticProps };
 
-const useQueryPost = () => {
-  // const { state } = useContext(MainContext);
-  const { toggle: loading, setToggle: setLoading } = useToggle();
-  const { searchVal } = useSearchByVal();
-  const [postList, setPostList] = useState<SinglePost[]>([]);
-
-  const handleQuery = async (searchVal: string) => {
-    if (!searchVal) {
-      setPostList([]);
-      return [];
-    }
-
-    try {
-      setLoading(true);
-      if (searchVal.includes('@')) {
-        const tag = searchVal.replace('@', '');
-        const queried = await queryArticleByTag(tag);
-        const articles = QueriedArticleHandlers.handleQueriedArticleList(
-          queried.data.articles
-        );
-        setPostList(articles);
-        return articles;
-      }
-      const queried = await queryArticleByVal(searchVal);
-      const articles = QueriedArticleHandlers.handleQueriedArticleList(
-        queried.data.articles
-      );
-      setPostList(articles);
-      return articles;
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-
-    // if (searchVal.includes('@')) {
-    //   const res = posts.filter((p) =>
-    //     p.tagList.includes(searchVal.replace('@', ''))
-    //   );
-    //   setPostList(res);
-    //   return res;
-    // }
-
-    // const res = posts.filter((p) => {
-    //   const contentsForSearch = [p.title, p.subTitle, p.content, p.description];
-    //   for (const c of contentsForSearch) {
-    //     if (c && c.includes(searchVal)) return true;
-    //   }
-    // });
-  };
-
-  useEffect(() => {
-    handleQuery(searchVal);
-  }, [searchVal]);
-
-  return {
-    loading,
-    searchVal: searchVal,
-    postList,
-    handleQuery,
-  };
-};
-
-export interface SearchedResultPageProps extends PostListViewProps { }
+export type SearchedResultPageProps = PostListViewProps;
 
 const SearchedResultPage = ({ postListData }: SearchedResultPageProps) => {
-  const loading = false;
-  // const { loading, searchVal, postList } = useQueryPost();
   const recommendedTagList = ['vue', 'nuxt3'];
 
   const hiddenPosts = postListData.map((p) => ({
@@ -127,7 +54,7 @@ const SearchedResultPage = ({ postListData }: SearchedResultPageProps) => {
         </Box>
       </Box>
       {postListData.length > 0 && <PostList postListData={hiddenPosts} />}
-      <Script> {`
+      <Script id="search-script"> {`
         (() => {
             const searchParams = new URLSearchParams(window.location.search);
             const searchVal = searchParams.get('q') || '';
@@ -172,23 +99,5 @@ const SearchedResultPage = ({ postListData }: SearchedResultPageProps) => {
     </Container>
   );
 };
-
-// export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => ({
-//   paths: [],
-//   fallback: false,
-//   // fallback: 'blocking',
-// });
-
-// export const getServerSideProps: GetServerSideProps<
-//   SearchedResultPageProps
-// > = async (ctx) => {
-//   const headTitle = `搜尋: ${ctx.params?.searchVal as string} | ${WEB_TITLE}`;
-
-//   return {
-//     props: {
-//       headTitle,
-//     },
-//   };
-// };
 
 export default memo(SearchedResultPage);

4c3bb71671b8064343c9115027efe3d05e9de7ac
PengiunCho <ms0223900@gmail.com>
2026-02-07 14:50:47 +0800

    feat: add ArticleMapper class to transform Article objects into SinglePost format for improved content handling

---
+++ lib/content/articleMapper.ts
@@ -0,0 +1,28 @@
+import { Article } from "@/lib/content/article.types";
+import { SinglePost } from "common-types";
+
+class ArticleMapper {
+  static toPost(article: Article): SinglePost {
+    return {
+      uid: article.id.toString(),
+      createdAt: article.publishedAt,
+      id: article.id.toString(),
+      title: article.title,
+      subTitle: article.subTitle || '',
+      description: article.description || '',
+      content: article.content,
+      thumbnail: article.thumbnailUrl ? {
+        src: article.thumbnailUrl,
+      } : null,
+      tagList: article.tags,
+      relatedArticleList: article.relatedArticles.map((relatedArticle) => ({
+        uid: relatedArticle.articleId,
+        id: relatedArticle.articleId,
+        title: relatedArticle.title,
+        subTitle: relatedArticle.subTitle || '',
+      })),
+    };
+  }
+}
+
+export default ArticleMapper;
